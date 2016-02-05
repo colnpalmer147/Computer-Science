53fd046d5c77a46abe44a89733633c3ba00abdb6
Palmer <colnpalmer147@gmail.com>
2016-02-04 23:14:09 -0500

    Intitial Check in

---
+++ String Interperator/src/Code/stringChanger.java
@@ -0,0 +1,16 @@
+package Code;
+
+public class stringChanger {
+	private String _input = null;
+	
+	public stringChanger(String input){
+		_input = input;
+	}
+
+
+	public String updateString(String input){
+		
+		
+		return input;
+	}
+}
